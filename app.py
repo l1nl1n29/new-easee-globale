ade5b7a1cad465e42d8cb8bb7fc74eecc12b83e1
Lin Lin Herlina <113963778+l1nl1n29@users.noreply.github.com>
2023-06-18 19:45:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def generate_image_from_base64(image_data):
     return filename
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
